a80c37ae5feb91027c98832cf226b998770cac60
titoValen <valentin.tito@davinci.edu.ar>
2025-12-01 14:24:03 -0300

    Agregar imágenes de productos y actualizar el renderizado en main.js

--- js/main.js
+++ js/main.js
@@ -7,30 +7,35 @@ const productos = [
     {
         id: 1,
         nombre: "Manzana",
+        imagen: "manzana.webp",
         categoria: "Frutas",
         precio: 100,
     },
     {
         id: 2,
         nombre: "Banana",
+        imagen: "banana.webp",
         categoria: "Frutas",
         precio: 80,
     },
     {
         id: 3,
         nombre: "Naranja",
+        imagen: "naranja.webp",
         categoria: "Frutas",
         precio: 90,
     },
     {
         id: 4,
         nombre: "Pera",
+        imagen: "pera.webp",
         categoria: "Frutas",
         precio: 110,
     },
     {
         id: 5,
         nombre: "Uva",
+        imagen: "uva.webp",
         categoria: "Frutas",
         precio: 120,
     },
@@ -38,30 +43,35 @@ const productos = [
     {
         id: 6,
         nombre: "Lechuga",
+        imagen: "lechuga.webp",
         categoria: "Verduras",
         precio: 70,
     },
     {
         id: 7,
         nombre: "Tomate",
+        imagen: "tomate.webp",
         categoria: "Verduras",
         precio: 95,
     },
     {
         id: 8,
         nombre: "Zanahoria",
+        imagen: "zanahoria.webp",
         categoria: "Verduras",
         precio: 60,
     },
     {
         id: 9,
         nombre: "Cebolla",
+        imagen: "cebolla.webp",
         categoria: "Verduras",
         precio: 50,
     },
     {
         id: 10,
-        nombre: "Pimiento",
+        nombre: "Morron",
+        imagen: "morron.webp",
         categoria: "Verduras",
         precio: 85,
     },
@@ -69,30 +79,35 @@ const productos = [
     {
         id: 11,
         nombre: "Champiñón",
+        imagen: "champiñon.webp",
         categoria: "Hongos",
         precio: 130,
     },
     {
         id: 12,
         nombre: "Portobello",
+        imagen: "portobello.webp",
         categoria: "Hongos",
         precio: 150,
     },
     {
         id: 13,
         nombre: "Shiitake",
+        imagen: "shiitake.webp",
         categoria: "Hongos",
         precio: 170,
     },
     {
         id: 14,
         nombre: "Enoki",
+        imagen: "enoki.webp",
         categoria: "Hongos",
         precio: 140,
     },
     {
         id: 15,
         nombre: "Ostra",
+        imagen: "ostra.webp",
         categoria: "Hongos",
         precio: 160,
     },
@@ -104,19 +119,51 @@ let listaProductos = $.getElementById("lista-productos");
 productos.forEach((prod) => {
     let productoDiv = $.createElement("div");
     productoDiv.classList.add("card");
+    
+    // Crear imagenes
+    let imagen = $.createElement("img");
+    imagen.src = `img/producto/${prod.imagen}`;
+    imagen.alt = prod.nombre;
 
+    // Crear nombre
     let nombre = $.createElement("h3");
     nombre.textContent = prod.nombre;
+    
+    // Crear contenido
+    let contenido = $.createElement("div");
+    contenido.classList.add("contenido");
 
     let categoria = $.createElement("p");
     categoria.textContent = `Categoría: ${prod.categoria}`;
-
+    
     let precio = $.createElement("p");
     precio.textContent = `Precio: $${prod.precio}`;
+    
+    contenido.appendChild(categoria);
+    contenido.appendChild(precio);
 
+    // Crear botón añadir al carrito
+    let conteinerBtn = $.createElement("div");
+    conteinerBtn.classList.add("conteiner-btn");
+
+    let botonAñadirCarrito = $.createElement("button");
+    botonAñadirCarrito.textContent = "Añadir carrito";
+    botonAñadirCarrito.classList.add("btn-añadir-carrito");
+
+    conteinerBtn.appendChild(botonAñadirCarrito);
+    
+    // Crear botón ver más
+    let botonVerMas = $.createElement("button");
+    botonVerMas.textContent = "Ver más";
+    botonVerMas.classList.add("btn-ver-mas");
+
+    conteinerBtn.appendChild(botonVerMas);
+
+    // Estructura del producto
+    productoDiv.appendChild(imagen);
     productoDiv.appendChild(nombre);
-    productoDiv.appendChild(categoria);
-    productoDiv.appendChild(precio);
+    productoDiv.appendChild(contenido);
+    productoDiv.appendChild(conteinerBtn);
 
     listaProductos.appendChild(productoDiv);
 });
